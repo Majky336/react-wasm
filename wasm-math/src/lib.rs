cdd8e18b25fd5d4601cee291fd06d44631fbdea4
Sojka Michal <sojka@cngroup.dk>
2021-07-07 16:31:18 +0200

    Generate wasm math package

---
+++ wasm-math/src/lib.rs
@@ -0,0 +1,32 @@
+mod utils;
+
+use wasm_bindgen::prelude::*;
+
+// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+// allocator.
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+#[wasm_bindgen]
+extern "C" {
+    fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn greet() {
+    alert("Hello, wasm-math!");
+}
+
+#[wasm_bindgen]
+pub fn add_two_ints(a: u32, b: u32) -> u32 {
+    a + b
+}
+
+#[wasm_bindgen]
+pub fn fibonaci(n: u32) -> u32 {
+    if n == 0 || n == 1 {
+        return n;
+    }
+    fibonaci(n - 1) + fibonaci(n - 2)
+}
